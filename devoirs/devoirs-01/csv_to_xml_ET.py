751ca3252f72577f3e78c42ceb2d86cf996fe12f
LucWCL <lucie.warckol@gmail.com>
2019-10-26 15:05:23 +0200

    devoirs relaxNG

--- devoirs/devoirs-01/csv_to_xml_ET.py
+++ devoirs/devoirs-01/csv_to_xml_ET.py
@@ -1,26 +1,42 @@
 import csv
-import xml.etree.ElementTree as ET
+from lxml import etree
 
-with open('tournagesdefilmsparis2011.csv','r') as csvfile, open('test_tournage.xml','w') as xmlfile:
-    readCSV = csv.reader(csvfile, delimiter=";")
+with open("tournagesdefilmsparis2011.csv","r",encoding="utf-8") as csvfile, open("test_tournage.xml","wb") as xmlfile:
+    tournage = csv.reader(csvfile, delimiter=";")
 
-    data=[]
-    for row in readCSV:
-        data.append(row)
-    #print(data)
-    n=0
+    next(tournage)
 
-    root = ET.Element('Shootings')
-    title = ET.SubElement(root,'Title',Status=f'{n}')
-    ET.SubElement(title, 'Director')
-    ET.SubElement(title, 'Lieu')
-    lieu = ET.SubElement(title, 'Adress')
-    ET.SubElement(lieu, 'Arrondissement')
-    ET.SubElement(lieu, 'Coordinates')
-    ET.SubElement(title, 'Producer_company')
-    ET.SubElement(title, 'Type')
-    date = ET.SubElement(title,'Date')
-    ET.SubElement(date,'Beggining')
-    ET.SubElement(date,'End')
+    root = etree.Element("Shootings")
+    for i, row in enumerate(tournage, start=1):
+        Film = etree.SubElement(root,"Film")
+        Film.set("n",str(i))
 
-    xmlfile.write(ET.tostring(root).decode())
+        Title = etree.SubElement(Film,"Title")
+        Title.text = row[0]
+
+        Director = etree.SubElement(Film, "Director")
+        Director.text = row[1]
+
+        Lieu = etree.SubElement(Film, "Lieu")
+        Adress = etree.SubElement(Lieu,"Adress")
+        Adress.text = row[2]
+        Arrondissement = etree.SubElement(Lieu,"Arrondissement")
+        Arrondissement.text = row[5]
+        Coordinates = etree.SubElement(Lieu,"Coordinates")
+        Coordinates.text = row[8]
+
+        Producer_company = etree.SubElement(Film,"Producer_company")
+        Producer_company.text = row[3]
+
+        Type = etree.SubElement(Film,"Type")
+        Type.text = row[4]
+
+        Date = etree.SubElement(Film,"Date")
+        Beggining = etree.SubElement(Date,"Beggining")
+        Beggining.text = row[6]
+        End = etree.SubElement(Date,"End")
+        End.text = row[7]
+
+    tree = etree.ElementTree(root)
+
+    tree.write(xmlfile, encoding="utf-8",xml_declaration=True, method="xml",pretty_print=True)
